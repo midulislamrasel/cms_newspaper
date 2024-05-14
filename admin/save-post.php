f4fe262cbadaff762c710926fee4de19ad3f2d5b
midulislamrasel <midulraddy121@gmail.com>
2024-05-15 01:22:46 +0600

    view page and category sesction

--- admin/save-post.php
+++ admin/save-post.php
@@ -3,6 +3,42 @@ include "../database/dbconfig.php";
 session_start();
 
 
+// if (isset($_FILES['fileToUpload'])) {
+//     $errors = array();
+
+//     $file_name = $_FILES['fileToUpload']['name'];
+//     $file_size = $_FILES['fileToUpload']['size'];
+//     $file_tmp = $_FILES['fileToUpload']['tmp_name'];
+//     $file_type = $_FILES['fileToUpload']['type'];
+//     $file_ext = strtolower(end(explode('.', $file_name)));
+//     $allowed_extensions = array("jpeg", "jpg", "png");
+
+
+
+//     if (!in_array($file_ext, $allowed_extensions)) {
+//         $errors[] = "Only JPG, JPEG, and PNG files are allowed.";
+//     }
+
+//     if ($file_size > 2097152) {
+//         $errors[] = "File size must be 2MB or lower.";
+//     }
+
+//     if (!empty($errors)) {
+
+//         move_uploaded_file($file_tmp, "../upload/" . $file_name);
+//     } else {
+//         foreach ($errors as $error) {
+//             echo "<div class='alert alert-danger'>$error</div>";
+//         }
+//         die();
+//     }
+// }
+
+
+
+// =============== test==========
+
+$newfilename = "newfilename";
 if (isset($_FILES['fileToUpload'])) {
     $errors = array();
 
@@ -10,11 +46,14 @@ if (isset($_FILES['fileToUpload'])) {
     $file_size = $_FILES['fileToUpload']['size'];
     $file_tmp = $_FILES['fileToUpload']['tmp_name'];
     $file_type = $_FILES['fileToUpload']['type'];
-    $file_ext = strtolower(end(explode('.', $file_name)));
+    $file_ext = strtolower(end(explode('.', $_FILES['fileToUpload']['name'])));
+    // $file_ext = strtolower(end(explode('.', $file_name)));
+
     $allowed_extensions = array("jpeg", "jpg", "png");
 
 
-    if (!in_array($file_ext, $allowed_extensions)) {
+
+    if (in_array($file_ext, $allowed_extensions) === false) {
         $errors[] = "Only JPG, JPEG, and PNG files are allowed.";
     }
 
@@ -22,15 +61,23 @@ if (isset($_FILES['fileToUpload'])) {
         $errors[] = "File size must be 2MB or lower.";
     }
 
-    if (empty($errors)) {
-        move_uploaded_file($file_tmp, "./upload/" . $file_name);
+    if (empty($errors) == true) {
+
+        // move_uploaded_file($file_tmp, "../upload/" . $file_name);
+        move_uploaded_file($file_tmp, "upload/" . $newfilename . "." . $file_ext);
     } else {
         foreach ($errors as $error) {
             echo "<div class='alert alert-danger'>$error</div>";
         }
-        die();
+        // die();
     }
 }
+
+
+
+// ===================
+
+
 
 $post_title = mysqli_real_escape_string($conn, $_POST['post_title']);
 $postdesc = mysqli_real_escape_string($conn, $_POST['postdesc']);
--- single.php
+++ single.php
@@ -1,36 +1,63 @@
-<?php include 'header.php'; ?>
-    <div id="main-content">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-8">
-                  <!-- post-container -->
-                    <div class="post-container">
-                        <div class="post-content single-post">
-                            <h3>Lorem ipsum dolor sit amet, consectetur</h3>
-                            <div class="post-information">
-                                <span>
-                                    <i class="fa fa-tags" aria-hidden="true"></i>
-                                    Html
-                                </span>
-                                <span>
-                                    <i class="fa fa-user" aria-hidden="true"></i>
-                                    <a href='author.php'>Admin</a>
-                                </span>
-                                <span>
-                                    <i class="fa fa-calendar" aria-hidden="true"></i>
-                                    01 Nov, 2019
-                                </span>
+<?php
+include 'header.php';
+include './database/dbconfig.php'
+?>
+<div id="main-content">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <!-- post-container -->
+                <div class="post-container">
+                    <?php
+
+                    $post_id = $_GET['id'];
+
+                    $sql = "SELECT * FROM post
+                     LEFT JOIN category ON post.category = category.category_id
+                     LEFT JOIN user ON post.author = user.user_id WHERE post_id = {$post_id}";
+
+
+
+                    $result = mysqli_query($conn, $sql) or die("Query Faild");
+
+                    if (mysqli_num_rows($result) > 0) {
+
+                        while ($row = mysqli_fetch_assoc($result)) {
+
+
+
+                    ?>
+                            <div class="post-content single-post">
+                                <h3><?php echo $row['title']; ?></h3>
+                                <div class="post-information">
+                                    <span>
+                                        <i class="fa fa-tags" aria-hidden="true"></i>
+                                        <a href="category.php?cid=<?php echo $row['category']; ?>">
+                                            <?php echo $row['category_name']; ?>
+                                        </a>
+
+                                    </span>
+                                    <span>
+                                        <i class="fa fa-user" aria-hidden="true"></i>
+                                        <a href='author.php'><?php echo $row['username']; ?></a>
+                                    </span>
+                                    <span>
+                                        <i class="fa fa-calendar" aria-hidden="true"></i>
+                                        <?php echo $row['post_date']; ?>
+                                    </span>
+                                </div>
+                                <img class="single-feature-image" src="images/post_1.jpg" alt="" />
+                                <p class="description">
+                                    <?php echo $row['description']; ?>
+                                </p>
                             </div>
-                            <img class="single-feature-image" src="images/post_1.jpg" alt=""/>
-                            <p class="description">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-                            </p>
-                        </div>
-                    </div>
-                    <!-- /post-container -->
+                    <?php }
+                    }; ?>
                 </div>
-                <?php include 'sidebar.php'; ?>
+                <!-- /post-container -->
             </div>
+            <?php include 'sidebar.php'; ?>
         </div>
     </div>
+</div>
 <?php include 'footer.php'; ?>
